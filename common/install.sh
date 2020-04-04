c8bfc69b48df8aa8326796a83c90ab6cab6f2b81
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2020-04-04 15:16:10 +0600

    Update install.sh

---
+++ common/install.sh
@@ -0,0 +1,4 @@
+  ui_print "***********************************"
+  ui_print "        GLTools Magisk EXT         "
+  ui_print "          Mod by Ahsan400          "
+  ui_print "***********************************"
